f7ac4c9c8482f1a999c1f7412dbc4586643f5aff
omichalcik <ondra.michalcik@volny.cz>
2022-01-19 11:34:15 +0100

    swag

--- src/main.c
+++ src/main.c
@@ -1,9 +1,11 @@
 #include "stm8s.h"
 #include "milis.h"
-
+#include "spse_stm8.h"
 /*#include "delay.h"*/
+
 #include <stdio.h>
-/*#include "uart1.h"*/
+#include "stm8s_adc2.h"
+#include "uart1.h"
 
 #define _ISOC99_SOURCE
 #define _GNU_SOURCE
@@ -24,15 +26,28 @@ void setup(void)
     CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);      // taktovani MCU na 16MHz
     GPIO_Init(LED_PORT, LED_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
     GPIO_Init(BTN_PORT, BTN_PIN, GPIO_MODE_IN_FL_NO_IT);
+    ADC2_SchmittTriggerConfig(ADC2_SCHMITTTRIG_CHANNEL4, DISABLE); //PB4
+    ADC2_SchmittTriggerConfig(ADC2_SCHMITTTRIG_CHANNEL5, DISABLE); //PB5
+    // nastavíme clock pro ADC (16MHz / 4 = 4MHz)
+    ADC2_PrescalerConfig(ADC2_PRESSEL_FCPU_D4);
+    // volíme zarovnání výsledku (typicky vpravo, jen vyjmečně je výhodné vlevo)
+    ADC2_AlignConfig(ADC2_ALIGN_RIGHT);
+    // nasatvíme multiplexer na některý ze vstupních kanálů
+    ADC2_Select_Channel(ADC2_CHANNEL_4);
+    // rozběhneme AD převodník
+    ADC2_Cmd(ENABLE);
+    // počkáme než se AD převodník rozběhne (~7us)
+    ADC2_Startup_Wait();
 
     init_milis();
-    /*init_uart1();*/
+    init_uart1();
 }
 
 
 int main(void)
 {
     uint32_t time = 0;
+    uint16_t ADCx = ADC_get(ADC2_CHANNEL_4);
 
     setup();
 
@@ -41,11 +56,11 @@ int main(void)
         if (milis() - time > 333 && BTN_PUSH) {
             LED_REVERSE; 
             time = milis();
-            /*printf("%ld\n", time);*/
+            printf("%ld %d\r\n", time, ADCx);
         }
 
-        /*LED_REVERSE; */
-        /*_delay_ms(333);*/
+        /*LED_REVERSE;*/
+        /*delay_ms(333);*/
         /*printf("Funguje to!!!\n");*/
     }
 }
